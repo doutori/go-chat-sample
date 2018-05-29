3e3ccb75e28ed2daa16e3a1ed6d368dcdd50b1cd
hiroaki.sasaki <sasa.hiro0125@gmail.com>
2018-05-30 01:33:14 +0900

    add chat, room

---
+++ room.go
@@ -0,0 +1,33 @@
+package main
+
+type room struct {
+	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
